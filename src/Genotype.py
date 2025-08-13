25f81c744ef432488b6b603e8a8f70a01517e45b
Katherine G. Skocelas <kgskocelas@gmail.com>
2025-08-13 17:57:45 -0400

    Add Genotype, Phenotype and WallPair class files to repo

---
+++ src/Genotype.py
@@ -0,0 +1,26 @@
+class Genotype:
+    """
+       A class representing an individual antenna's genotype.
+
+       Attributes:
+
+           num_wall_pairs (int): the # of wall-pairs of an individual horn antenna.
+           height (float): the height of the antenna.
+           waveguide_length (float): the length of the waveguide.
+           waveguide_height (float): the height of the waveguide.
+           wall_pairs (
+
+       """
+
+
+
+
+
+
+
+
+
+
+
+    def __init__(self, genotypes):
+        self.genotypes = genotypes
