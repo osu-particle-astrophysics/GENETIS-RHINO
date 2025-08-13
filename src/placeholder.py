a5b7e9ade3b7cff14c1fd162289c111ed5d8f7dc
Emily Dolson <emilyldolson@gmail.com>
2025-08-13 17:34:25 -0400

    Create placeholder.py

---
+++ src/placeholder.py
@@ -0,0 +1,6 @@
+def my_function(my_input):
+    """
+    This is a placeholder function that does nothing.
+    It is here to test linting, but it can be removed once we have other files.
+    """
+    pass
