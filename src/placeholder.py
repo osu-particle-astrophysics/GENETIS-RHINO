2f3d621ba2bccb4fe25eb2a1f3bbbb3dfa0e7893
Emily Dolson <emilyldolson@gmail.com>
2025-08-13 17:54:05 -0400

    Fix extra space

--- src/placeholder.py
+++ src/placeholder.py
@@ -7,7 +7,7 @@ We can remove it once we have other files.
 def my_function(my_input: int) -> None:
     """
     This is a placeholder function that does nothing.
-    
+
     It is here to test linting, but it can be removed once we have other files.
     """
 
