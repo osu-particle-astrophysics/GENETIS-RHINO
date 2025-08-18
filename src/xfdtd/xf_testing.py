e2c05902c436e52b33061edd78d451848a361356
jacobweiler <zed8drift@gmail.com>
2025-08-18 09:57:33 -0400

    Adding xfdtd directories + test files

---
+++ src/xfdtd/xf_testing.py
@@ -0,0 +1,9 @@
+"""File to run XFdtd simulation tests to make sure files work."""
+
+# Put temporary genes for testing
+
+# Function for setting up simulation setup + building scripts
+
+# Function to submit xf simulation job
+
+# Function to output xf files (uan and s11)
